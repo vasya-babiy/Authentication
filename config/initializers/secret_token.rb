22f017b4c4cc693caf6431f83ab6189ea82b5c1f
vasya-babiy <salequalitypurse@gmail.com>
2014-04-22 00:51:41 +0300

    123

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ath::Application.config.secret_token = '84b80d0d651ae8ef1c2aabe19c7ff16f38430a44978186d5d2becb58ad480f13c5fc2f51d90fb32cd163849fe83c8a2b44f2c2bf34caa64ef30c11e1977b2dec'
